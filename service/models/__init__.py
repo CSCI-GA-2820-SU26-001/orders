563b41ec3d412f6cb6b801e402c215716203b7f1
anerivs <aneri.shah@nyu.edu>
2026-06-29 12:37:17 -0400

    Query feature and pylint

--- service/models/__init__.py
+++ service/models/__init__.py
@@ -1,3 +1,7 @@
+"""
+Service model package exports persistent base objects and domain models.
+"""
+
 from .persistent_base import db, DataValidationError
 from .order import Order
 from .order_items import OrderItem
